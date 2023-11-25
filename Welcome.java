2ebe59c2e25a251c34d3f12042de286817745327
kondwani padyera <kondwanipadyera@gmail.com>
2023-11-25 12:31:50 -0800

    added a semicolon at the end of the println statement

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
 //First Java Program To Be Pushed On GitHub
 public class Welcome{
 	public static void main(String[] args){
-		System.out.println("This is my First Java Program to be pushed on this repo")
+		System.out.println("This is my First Java Program to be pushed on this repo");
 	}
 }
