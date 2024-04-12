16831a0726b2ddbf88e5db692536a4f934889b49
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-12 10:54:23 +0200

    Add agreement status check based on user input

---
+++ PYTHON/Agree.py
@@ -0,0 +1,9 @@
+s = input("Do you agree?: ")
+
+#  Check if the user agreed or not
+if s == 'y' or s == 'Y':
+    print("Agreed!")
+elif s == 'n' or s == 'N':
+    print("Not Agreed!!")
+else:
+    print("Y for yes and N for no")
