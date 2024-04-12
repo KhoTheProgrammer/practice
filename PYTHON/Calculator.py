73d12fa44b3f2a4bca787d05f463c76b0aa68794
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-12 12:22:18 +0200

    Implement get_int function for user input handling

--- PYTHON/Calculator.py
+++ PYTHON/Calculator.py
@@ -1,6 +1,17 @@
-#  Get input from user
-x = int(input("Enter a number: "))
-y = int(input("Enter a number: "))
+def main():
+    x = get_int("Enter a number: ")
+    y = get_int("Enter a number: ")
+    # Prints sum of two numbers
+    print (x + y)
+    
+ 
+# Returns a number from user otherwise prompts for a number
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Not a number")
+            
+main()
 
-# Prints the sum
-print("The sum of", x, "and", y, "is", x + y)
