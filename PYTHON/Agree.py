4d4166a8d3a1c3e9f1c62bbc3f4ac90518313e04
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-12 11:04:30 +0200

    Improve input handling for agreement status check  -Add on line 3 comment to improve readability

--- PYTHON/Agree.py
+++ PYTHON/Agree.py
@@ -1,9 +1,12 @@
 s = input("Do you agree?: ")
 
+# Converts input to lowercases
+s = s.lower()
+
 #  Check if the user agreed or not
-if s in ["y", "Yes"]:
+if s in ["y", "yes"]:
     print("Agreed!")
-elif s in ["n", "No"]:
+elif s in ["n", "no"]:
     print("Not Agreed!!")
 else:
     print("Y for yes and N for no")
