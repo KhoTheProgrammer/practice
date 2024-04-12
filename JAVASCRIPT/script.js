8db3401b888cefd6db9872c69623216d25100fb3
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-12 07:57:34 +0200

    Implement sayHello function for user interaction

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,2 +1,15 @@
-// Prints hello world to the console
-console.log("Hello, World!!");
+// // Prints hello world to the console
+// console.log(document.getElementById("title"));
+function sayHello(){
+    let name = document.getElementById("name").value;
+    let message = '<h2>hello ' + name + '!</h2>';
+    document.getElementById('content').innerHTML = message;
+    
+    if (name == 'student')
+    {
+        let title = document.querySelector("#title").textContent;
+        title += " & loving it";
+        document.getElementById("title").textContent = title;
+    }
+}
+document.querySelector("button").addEventListener("click", sayHello);
