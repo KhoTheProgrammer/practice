6b1c633a6e5ae0468101761861dc916798a8bea8
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-12 10:42:20 +0200

    Implement string comparison functionality

---
+++ PYTHON/Compare.py
@@ -0,0 +1,9 @@
+# Get two strings from user
+x = input("Enter a word: ")
+y = input("Enter a word: ")
+
+# Check if the two words are equal or not
+if x == y:
+    print("Same")
+else:
+    print("Different")
