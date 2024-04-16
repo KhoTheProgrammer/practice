e8303d0b2a8fe00de96516685908658d91a939d9
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-16 14:58:27 +0200

    Add temperature conversion functionality
    - Implemented JavaScript code for a basic temperature converter.
    - Allows users to input a temperature value and choose between Celsius and Fahrenheit.
    - Converts the input temperature accordingly and displays the result.

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,15 +1,36 @@
-// // Prints hello world to the console
-// console.log(document.getElementById("title"));
-function sayHello(){
-    let name = document.getElementById("name").value;
-    let message = '<h2>hello ' + name + '!</h2>';
-    document.getElementById('content').innerHTML = message;
+// Get references to HTML elements
+let button = document.getElementById("button"); // Button element
+let textField = document.getElementById("tfield"); // Input field for temperature
+let radioCelcius = document.getElementById("cRadio"); // Celsius radio button
+let radioFahren = document.getElementById("fRadio"); // Fahrenheit radio button
+let result = document.getElementById("result"); // Result display area
+
+// Event handler for button click
+button.onclick = function(){
+    // Get the value entered by the user and convert it to a number
+    let number = Number(textField.value);
     
-    if (name == 'student')
-    {
-        let title = document.querySelector("#title").textContent;
-        title += " & loving it";
-        document.getElementById("title").textContent = title;
+    // Check which radio button is selected
+    if (radioCelcius.checked){
+        // If Celsius is selected, convert the temperature to Celsius and display the result
+        result.innerHTML = toCelcius(number) + "°C";
+    }
+    else if (radioFahren.checked){
+        // If Fahrenheit is selected, convert the temperature to Fahrenheit and display the result
+        result.innerHTML = toFahren(number) + "°F";
+    }
+    else{
+        // If neither radio button is selected, prompt the user to select one
+        result.innerHTML = "Please select one";
     }
 }
-document.querySelector("button").addEventListener("click", sayHello);
+
+// Function to convert temperature to Fahrenheit
+function toFahren(temp){
+    return (temp * 9 / 5) + 32;
+}
+
+// Function to convert temperature to Celsius
+function toCelcius(temp){
+    return (temp - 32) * (5 / 9);
+}
