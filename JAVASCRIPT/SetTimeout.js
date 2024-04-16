4d7eb696de61ee8a01346f90992c4cedabed5d5c
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-16 16:28:58 +0200

    Schedule delayed function executions using setTimeout
    - Utilize setTimeout to schedule the execution of three functions after specified intervals.
    - Display different messages using alert in each scheduled function.
    - Achieve staggered message display on the page.

---
+++ JAVASCRIPT/SetTimeout.js
@@ -0,0 +1,23 @@
+// Schedule the execution of the firstMessage function after 3000 milliseconds (3 seconds)
+setTimeout(firstMessage, 3000);
+
+// Schedule the execution of the secondMessage function after 6000 milliseconds (6 seconds)
+setTimeout(secondMessage, 6000);
+
+// Schedule the execution of the thirdMessage function after 9000 milliseconds (9 seconds)
+setTimeout(thirdMessage, 9000);
+
+// Function to display the first message
+function firstMessage(){
+    alert("My name is kondwani");
+}
+
+// Function to display the second message
+function secondMessage(){
+    alert("I am from Malawi");
+}
+
+// Function to display the third message
+function thirdMessage(){
+    alert("I am in my second year at UNIMA");
+}
