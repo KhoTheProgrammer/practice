3d81e064e3d41417f2401804e7698613f90fcfe1
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-17 20:55:20 +0200

    Add a functionality of alerting the user when a button is clicked

---
+++ JAVASCRIPT/Events.js
@@ -0,0 +1,8 @@
+// let button = document.getElementById("mybutton");
+// button.onclick = sayHello;
+
+let element = document.body;
+element.onload = sayHello;
+function sayHello(){
+    alert("You did something");
+}
