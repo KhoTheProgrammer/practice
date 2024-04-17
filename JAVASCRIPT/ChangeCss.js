f8f26c5e310e31625dc427f410a4eb7c8322d3fc
kondwani padyera <kondwanipadyera@gmail.com>
2024-04-17 20:42:48 +0200

    Add a javascript program to change the css of a web page

---
+++ JAVASCRIPT/ChangeCss.js
@@ -0,0 +1,6 @@
+let title = document.getElementById("mytitle");
+
+title.style.backgroundColor = "rgb(50, 200, 250";
+title.style.color = "white";
+title.style.textAlign = "center";
+title.style.padding = "10px";
