d24dc8307cee1bc566889c069e428efb7d708920
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-06-19 12:06:17 +0200

    Modified a file

--- C++/call.cpp
+++ C++/call.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "my name is blessings " << endl;
-    return 0;
+void add(int *a, int *b);
+
+int main()
+{
+    int a = 5, b = 7;
+    cout << "**********Before********" << endl;
+    cout << "a: " << a << endl
+         << "b: " << b << endl;
+    add(&a, &b);
+    cout << "**********After********" << endl;
+    cout << "a: " << a << endl
+         << "b: " << b << endl;
+}
+
+void add(int *a, int *b)
+{
+    *a = 10;
+    *b = 10;
 }
