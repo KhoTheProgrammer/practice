9c620a92bfdf54f2a28b7221768a392999098140
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-06-22 19:11:36 +0200

    Added text to the output

--- PYTHON/Test.py
+++ PYTHON/Test.py
@@ -2,7 +2,7 @@ def main():
     num1 = 8
     num2 = 5
     result = add(num1, num2)
-    print(result)
+    print("The sum of" , num1 , "and" , num2 , "is:" , result)
     
 def add(a, b):
     return a + b
